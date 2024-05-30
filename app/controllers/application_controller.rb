b6a5e834164cbe02e52642b9571a2961119d38e1
Fai Wong <wongwf82@gmail.com>
2024-05-31 00:19:13 +0800

    Add user login validation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper
+
+  before_action :require_login
+
+  private
+
+  def require_login
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,22 @@
 class SessionsController < ApplicationController
+  before_action :require_login, except: [:new, :create]
+
   def new
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    user = User.find_by(email: params[:email].downcase)
+    if user && user.authenticate(params[:password])
       log_in user
-      redirect_to user
+      redirect_to products_url
     else
-      render 'new'
+      flash.now[:errors] = "Invalid email/password combination"
+      render "new", status: :unprocessable_entity
     end
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
